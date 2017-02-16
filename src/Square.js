6453063e7e3f2754e36d898b8532086d00d8b07d
Federico Rota <federico.rota01@gmail.com>
2017-02-17 00:01:39 +0100

    Convert to stateless functional component

--- src/Square.js
+++ src/Square.js
@@ -1,18 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Square extends Component {
-	constructor(){
-		super();
-		this.state = {
-			value: null,
-		};
-	}
-	render() {
+function Square(props){
 		return (
-			<button className="square" onClick={() => this.props.onClick()}>
-			{this.props.value}
+			<button className="square" onClick={() => props.onClick()}>
+			{props.value}
 			</button>
 		)
-	}
 }
 export default Square;
